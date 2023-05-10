807fb124bd62a05226532e2c2d222f040d92ed69
Hanun <justhanun@yahoo.com>
2023-05-10 10:21:12 +0700

    Initial Commit

---
+++ CV Hanun/CV_HanunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CV_HanunApp.swift
+//  CV Hanun
+//
+//  Created by Kevin Maulana on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CV_HanunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
